63b2d9013ea05ee1b7bbe0a21ac5ed79ee4785ab
Lyndsey Dong <Lyndsey.A.Dong-1@ou.edu>
2025-02-28 17:19:08 -0600

    Minor changes to organization, file names, and relinking

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,4 @@
+// Movement to the login page
+  document.querySelector(".loginButton").addEventListener("click", function() {
+    window.location.href = "../authentication/authentication.html";
+  });
