44947e4e50df42deda30d37f461321abc23f9e11
Kyumin Lee <2km4890@gmail.com>
2025-04-09 14:46:12 -0500

    fixing login routing

--- assets/js/pages/welcome.js
+++ assets/js/pages/welcome.js
@@ -1,4 +1,4 @@
 // Movement to the login page
   document.querySelector(".loginButton").addEventListener("click", function() {
-    window.location.href = "../authentication/authentication.html";
+    window.location.href = "../pages/authentication/authentication.html";
   });
