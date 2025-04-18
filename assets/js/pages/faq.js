030e61a43a4038eb6a28c0f71162c229d365ea16
Kyumin Lee <2km4890@gmail.com>
2025-04-18 16:03:49 -0500

    separating faq

---
+++ assets/js/pages/faq.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+    
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', () => {
+            const faqItem = question.parentElement;
+            faqItem.classList.toggle('active');
+        });
+    });
+});
